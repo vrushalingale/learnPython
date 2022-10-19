453e62c565291a9bd91cf190ec9cb2a7dcd61733
vrushalingale <ingalevrushal@gmail.com>
2022-10-19 19:10:01 +0530

    commitZTM 19 Oct

---
+++ Python_ZTM_Code/PythonOOPs/PlayCharacters.py
@@ -0,0 +1,6 @@
+class PlayCharacters:
+    def letsCompleteCourse(course):
+        print(f'You can Complete {course}, you have the Guts')
+
+player1 = PlayCharacters()
+player1.letsCompleteCourse('Vrushal')
